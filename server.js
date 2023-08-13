dbce14a2a0a5504a9f5c0a6046513840894e7bdc
Arsene Fokam P <2210100@isi-montreal.com>
2023-08-13 13:11:05 -0400

    server running

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const colors = require("colors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+
+// rest object 
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+// routes
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message : "server running",
+    })
+})
+
+// Listen port 
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`server running in ${process.env.NODE_MODE} Mode on port ${port}`.bgCyan.white)
+})
