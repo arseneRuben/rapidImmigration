d709686f06c5368fdd9fe4ab7ce60f3b273ec031
Arsene Fokam P <2210100@isi-montreal.com>
2023-09-25 17:08:47 -0400

    redux action on folder

---
+++ client/src/actions/folder.js
@@ -0,0 +1,87 @@
+import { START_LOADING, END_LOADING, FETCH_ALL_FOLDERS, FETCH_FOLDER, FETCH_BY_CLIENT_FOLDER, CREATE_FOLDER, UPDATE_FOLDER, DELETE_FOLDER, FETCH_BY_CONSULTANT_FOLDER, FETCH_BY_CLIENT_FOLDER } from '../constants/actionTypes';
+import * as api from '../api/index.js';
+import {  message } from 'antd'
+
+export const getFolders = () => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchFolders();
+    dispatch({ type: FETCH_ALL_FOLDERS, payload: data });
+    dispatch({ type: END_LOADING });
+} catch (error) {
+    message.error(error.message)
+}
+}
+
+export const getFolder = (id) => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchFolder(id);
+    dispatch({ type: FETCH_FOLDER, payload: { folder: data } });
+ } catch (error) {
+    message.error(error.message)
+}
+}
+
+export const createFolder = (folder, navigate) => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.createFolder(folder);
+    dispatch({ type: CREATE_FOLDER, payload: data });
+    message.success('Folder created');
+    navigate(`/folders`);
+} catch (error) {
+    message.error(error.message)
+}
+}
+
+export const updateFolder = (folder, navigate) => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.updateFolder(folder);
+    dispatch({ type: UPDATE_FOLDER, payload: data });
+    message.success('Folder updated');
+    navigate(`/folders`);
+ } catch (error) {
+    message.error(error.message)
+}
+}
+
+
+export const getFoldersByClient = (name) => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    const { data: { data } } = await api.fetchFoldersByClient(name);
+    dispatch({ type: FETCH_BY_CLIENT_FOLDER, payload: { data } });
+    dispatch({ type: END_LOADING });
+ } catch (error) {
+    message.error(error.message)
+}
+}
+
+export const getFoldersByConsultant = (name) => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    const { data: { data } } = await api.fetchFoldersByConsultant(name);
+    dispatch({ type: FETCH_BY_CONSULTANT_FOLDER, payload: { data } });
+    dispatch({ type: END_LOADING });
+ } catch (error) {
+    message.error(error.message)
+}
+}
+
+
+export const deleteFolder = (id, navigate) => async (dispatch) => {
+    try {
+    dispatch({ type: START_LOADING });
+    await api.deleteFolder(id);
+    dispatch({ type: DELETE_FOLDER, payload: id });
+    message.success('Folder deleted successfully');
+    navigate('/folders');
+ } catch (error) {
+    message.error(error.message);
+}
+}
+
+
+
