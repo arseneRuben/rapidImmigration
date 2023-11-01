00645b6e0310ae8790a140e9b31a170b3f6d341d
arsene <fopoar@gmail.com>
2023-11-01 12:20:33 -0400

    baseUrl

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const baseURL = 'http://localhost:8080';
+export const baseURL = '';
 const API = axios.create({ baseURL:`${baseURL}/api`});
 
 API.interceptors.request.use((req) => {
